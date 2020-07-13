5bd64afec1fb40629ae89ff930dd87aac58efba6
ZekeZR1 <hnta3574@gmail.com>
2020-07-13 11:38:22 +0900

    add : indicator

---
+++ Assets/Scripts/ARTapToPlace.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARTapToPlace : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    private ARSessionOrigin arOrigin;
+    private bool placementPoseIsValid = false;
+    private Pose PlacementPose;
+
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        var raycasttManager = arOrigin.GetComponent<ARRaycastManager>();
+        raycasttManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            PlacementPose = hits[0].pose;
+        }
+    }
+}
+
