d74020d381d348c69108e41311130859b629ac82
ZekeZR1 <hnta3574@gmail.com>
2020-07-15 11:16:31 +0900

    hoge

--- Assets/Scripts/ARTapToPlace.cs
+++ Assets/Scripts/ARTapToPlace.cs
@@ -3,24 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 
 public class ARTapToPlace : MonoBehaviour
 {
+    public GameObject objectToPlace;
+
     public GameObject placementIndicator;
     private ARSessionOrigin arOrigin;
     private bool placementPoseIsValid = false;
     private Pose PlacementPose;
+    public GameObject text;
+    private ARRaycastManager raycasttManager;
 
+    [SerializeField]
+    GameObject arCamera = default;
 
     void Start()
     {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
+        raycasttManager = arOrigin.GetComponent<ARRaycastManager>();
     }
 
     void Update()
     {
+        
         UpdatePlacementPose();
         UpdatePlacementIndicator();
+
+        //calc dot
+        var arcrot = arCamera.transform.rotation;
+
+        text.GetComponent<Text>().text = "";
+
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Instantiate(objectToPlace, PlacementPose.position, PlacementPose.rotation);
+        }
     }
 
     private void UpdatePlacementIndicator()
@@ -29,10 +49,12 @@ public class ARTapToPlace : MonoBehaviour
         {
             placementIndicator.SetActive(true);
             placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+            //text.GetComponent<Text>().text = PlacementPose.position.ToString();
         }
         else
         {
             placementIndicator.SetActive(false);
+            //text.GetComponent<Text>().text = "the ray doesn't hit anything lol";
         }
     }
 
@@ -40,12 +62,14 @@ public class ARTapToPlace : MonoBehaviour
     {
         var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
         var hits = new List<ARRaycastHit>();
-        var raycasttManager = arOrigin.GetComponent<ARRaycastManager>();
-        raycasttManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        raycasttManager.Raycast(screenCenter, hits, TrackableType.All);
         placementPoseIsValid = hits.Count > 0;
         if (placementPoseIsValid)
         {
             PlacementPose = hits[0].pose;
+            var cF = Camera.current.transform.forward;
+            var cB = new Vector3(cF.x, 0, cF.z).normalized;
+            PlacementPose.rotation = Quaternion.LookRotation(cB);
         }
     }
 }
---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class CreateObject : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectPrefab;
+    public GameObject text;
+
+    ARRaycastManager raycastManager;
+    List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
+    // 初期化時に呼ばれる
+    void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // フレーム毎に呼ばれる
+    void Update()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        if (raycastManager.Raycast(screenCenter, hitResults, TrackableType.All))
+        {
+            text.GetComponent<Text>().text = "Raycast working";
+        }
+        else
+        {
+            text.GetComponent<Text>().text = "Raycast not working";
+        }
+        if(hitResults.Count > 0)
+        {
+            text.GetComponent<Text>().text = hitResults[0].pose.position.ToString();
+        }
+    }
+}
