3f37c89c163756e11726446671c243ebb2c847d0
ZekeZR1 <hnta3574@gmail.com>
2020-07-16 12:30:24 +0900

    add : accuracy calc

--- Assets/Scripts/GameManagement/GameManager.cs
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject accTextObj;
 
+    private int totalNotes = 0;
+    private float hittenNotes = 0f;
+    private float totalAccuracy = 0f;
 
     void Start()
     {
-        
+            
     }
 
     void Update()
     {
         
     }
+
+    public void CalcCurrentAccuracy(float acc)
+    {
+        totalNotes++;
+
+        if (acc >= 0.95f)
+        {
+            //Perfect
+            hittenNotes += 1f;
+        }
+        else if (acc >= 0.9f)
+        {
+            //Great
+            hittenNotes += 0.8f;
+        }
+        else if (acc >= 0.85f)
+        {
+            //Good
+            hittenNotes += 0.5f;
+        }
+        else
+        {
+            //Miss
+            hittenNotes += 0f;
+        }
+
+        totalAccuracy = (hittenNotes / totalNotes);
+        accTextObj.GetComponent<Text>().text = totalAccuracy.ToString();
+    }
+
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,26 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    GameObject gameManagerObj = default;
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = gameManagerObj.GetComponent<GameManager>();
+    }
+
     private void OnTriggerEnter(Collider col)
     {
-        if (col.tag == "Bullet")
-        {
+        if (col.tag == "Bullet") { 
             var toObjectVec = col.gameObject.transform.position - this.transform.position;
             var forwardVec = this.transform.forward;
             var deg = Vector3.Dot(toObjectVec.normalized, forwardVec.normalized);
-            if (deg > 0.7f)
-            {
-                Debug.Log("blocked!!");
-            }
-            else
-            {
-                Debug.Log("Dead!!");
-            }
+            gameManager.CalcCurrentAccuracy(deg);
         }
     }
 }
