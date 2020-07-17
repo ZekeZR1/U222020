9223ba17e351ccf77f41c6f1fa1b8ab670fdd800
拠点斗 <kyotento0507@gmail.com>
2020-07-17 16:41:20 +0900

    えんだあああああああああああああああああああああああああああいああああああああああああ

--- Assets/Scripts/ConstantVelocityAttack.cs
+++ Assets/Scripts/ConstantVelocityAttack.cs
@@ -5,26 +5,51 @@ using UnityEngine;
 public class ConstantVelocityAttack : MonoBehaviour
 {
     [SerializeField] Transform endPos;  //終点座標
-    [SerializeField] float flightTime = 2;  //滞空時間
+    [SerializeField] float flightTime = 2;  //到達時間。
     [SerializeField] float speedRate = 1;   //滞空時間を基準とした移動速度倍率
     private const float gravity = -9.8f;    //重力
+    [SerializeField] float heightRate = 1;  //投てきの高さの倍率。
+    /// <summary>
+    /// 射出するオブジェクト
+    /// </summary>
+    [SerializeField, Tooltip("射出するオブジェクトをここに割り当てる")]
+    private GameObject ThrowingObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Jump(endPos.position, flightTime, speedRate, gravity));
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //todo 仮。
+        //左クリックした時、玉を上から発射する。
+        if (Input.GetMouseButtonDown(0))        
+        {
+            StartCoroutine(Jump(endPos.position, flightTime, speedRate, gravity * heightRate));
+        }
+        //右クリックした時、玉を正面から発射する。
+        if(Input.GetMouseButtonDown(1))
+        {
+           StartCoroutine(ForwardAttack(endPos.position, flightTime, speedRate));
+        }
+
+        //マウスホイールを押し込んだ時。
+        if(Input.GetMouseButtonDown(2))
+        {
+         //   StartCoroutine(RightAttackes(endPos.position, flightTime, speedRate, gravity * heightRate));
+            StartCoroutine(LeftAttack(endPos.position, flightTime, speedRate, gravity * heightRate));
+        }
+
     }
 
     // 現在位置からendPosへの放物運動　
     private IEnumerator Jump(Vector3 endPos, float flightTime, float speedRate, float gravity)
     {
+        //// Ballオブジェクトの生成
+        GameObject ball = Instantiate(ThrowingObject, this.transform.position, Quaternion.identity);
 
         var startPos = transform.position; // 初期位置
         var diffY = (endPos - startPos).y; // 始点と終点のy成分の差分
@@ -35,10 +60,147 @@ public class ConstantVelocityAttack : MonoBehaviour
         {
             var p = Vector3.Lerp(startPos, endPos, t / flightTime);   //水平方向の座標を求める (x,z座標)
             p.y = startPos.y + vn * t + 0.5f * gravity * t * t; // 鉛直方向の座標 y
-            transform.position = p;
+            ball.transform.position = p;
             yield return null; //1フレーム経過
         }
         // 終点座標へ補正
-        transform.position = endPos;
+        ball.transform.position = endPos;
+    }
+
+    //左攻撃。
+    private IEnumerator LeftAttacks(Vector3 endPos, float flightTime, float speedRate, float gravity)
+    {
+        //// Ballオブジェクトの生成
+        GameObject ball = Instantiate(ThrowingObject, this.transform.position, Quaternion.identity);
+
+        var startPos = transform.position; // 初期位置
+        var diffX = (endPos - startPos).x; // 始点と終点のy成分の差分
+        var diffZ = (endPos - startPos).z; // 始点と終点のy成分の差分
+        var vnx = (diffX - gravity * 0.5f * flightTime * flightTime) / flightTime; // 鉛直方向の初速度vn
+        var vnz = (diffZ - gravity * 0.5f * flightTime * flightTime) / flightTime; // 鉛直方向の初速度vn
+
+        //X成分の差のほうが大きいとき。
+        if (diffX > diffZ)
+        {
+            // 放物運動
+            for (var t = 0f; t < flightTime; t += (Time.deltaTime * speedRate))
+            {
+                var p = Vector3.Lerp(startPos, endPos, t / flightTime);   //水平方向の座標を求める (x,z座標)
+                p.x = startPos.x + vnx * t + 0.5f * gravity * t * t; // 鉛直方向の座標 y
+                ball.transform.position = p;
+                yield return null; //1フレーム経過
+            }
+        }
+        //Z成分の差のほうが大きいとき。
+
+        if (diffX < diffZ)
+        {
+            // 放物運動
+            for (var t = 0f; t < flightTime; t += (Time.deltaTime * speedRate))
+            {
+                var p = Vector3.Lerp(startPos, endPos, t / flightTime);   //水平方向の座標を求める (x,z座標)
+                p.z = startPos.z - vnz * t - 0.5f * gravity * t * t; // 鉛直方向の座標 y
+                ball.transform.position = p;
+                yield return null; //1フレーム経過
+            }
+        }
+        
+        // 終点座標へ補正
+        ball.transform.position = endPos;
+    }
+
+    //右攻撃。
+    private IEnumerator RightAttackes(Vector3 endPos, float flightTime, float speedRate, float gravity)
+    {
+        //// Ballオブジェクトの生成
+        GameObject ball = Instantiate(ThrowingObject, this.transform.position, Quaternion.identity);
+
+        var startPos = transform.position; // 初期位置
+        var diffX = (endPos - startPos).x; // 始点と終点のy成分の差分
+        var diffZ = (endPos - startPos).z; // 始点と終点のy成分の差分
+        var vnx = (diffX - gravity * 0.5f * flightTime * flightTime) / flightTime; // 鉛直方向の初速度vn
+        var vnz = (diffZ - gravity * 0.5f * flightTime * flightTime) / flightTime; // 鉛直方向の初速度vn
+
+        //X成分の差のほうが大きいとき。
+        if (diffX > diffZ)
+        {
+            // 放物運動
+            for (var t = 0f; t < flightTime; t += (Time.deltaTime * speedRate))
+            {
+                var p = Vector3.Lerp(startPos, endPos, t / flightTime);   //水平方向の座標を求める (x,z座標)
+                p.x = startPos.x - vnx * t - 0.5f * gravity * t * t; // 鉛直方向の座標 y
+                ball.transform.position = p;
+                yield return null; //1フレーム経過
+            }
+        }
+        //Z成分の差のほうが大きいとき。
+        if (diffX < diffZ)
+        {
+            // 放物運動
+            for (var t = 0f; t < flightTime; t += (Time.deltaTime * speedRate))
+            {
+                var p = Vector3.Lerp(startPos, endPos, t / flightTime);   //水平方向の座標を求める (x,z座標)
+                p.z = startPos.z - vnz * t - 0.5f * gravity * t * t; // 鉛直方向の座標 y
+                ball.transform.position = p;
+                yield return null; //1フレーム経過
+            }
+        }
+
+        // 終点座標へ補正
+        ball.transform.position = endPos;
+    }
+
+    //左攻撃(諦めたやつ)。
+    private IEnumerator LeftAttack(Vector3 endPos, float flightTime, float speedRate, float gravity)
+    {
+        //// Ballオブジェクトの生成
+        GameObject ball = Instantiate(ThrowingObject, this.transform.position, Quaternion.identity);
+
+        var startPos = transform.position; // 初期位置
+        var diff = endPos - startPos; // 始点と終点のy成分の差分; // 始点と終点のx成分の差分
+
+        Vector3 y = new Vector3(0, 1, 0);     //敵の直上ベクトル。
+        var crossVec = Vector3.Cross(diff, y);        //外積を使ってプレイヤー方向の垂線を求める。
+        var crossVecNormalize = crossVec.normalized;                //正規化。
+        //todo 逆にするときはがいせきのあたいをぎゃくにしてあげるうｐ。
+        var vn = (/*diffY - */gravity * 0.5f * flightTime * flightTime) / flightTime; // 鉛直方向の初速度vn
+
+        // 放物運動
+        for (var t = 0f; t < flightTime; t += (Time.deltaTime * speedRate))
+        {
+            var p = Vector3.Lerp(startPos, endPos, t / flightTime);   //水平方向の座標を求める (x,z座標)
+            p += crossVecNormalize * (vn * t - 0.5f * gravity * t * t);// 鉛直方向の座標 x
+            ball.transform.position = p;
+            yield return null; //1フレーム経過
+        }
+        // 終点座標へ補正
+        ball.transform.position = endPos;
+    }
+
+
+    /// <summary>
+    /// 正面の攻撃。
+    /// </summary>
+    /// <param name="endPos">終点</param>
+    /// <param name="flightTime">敵に玉が衝突するまでの時間</param>
+    /// <param name="speedRate">スピード倍率</param>
+    private IEnumerator ForwardAttack(Vector3 endPos, float flightTime, float speedRate)
+    {
+        // Ballオブジェクトの生成
+        GameObject ball = Instantiate(ThrowingObject, transform.position, Quaternion.identity);
+
+        var startPos = this.transform.position;                         //初期座標。
+        var distance = endPos - startPos;                               //玉までの距離。    
+        var processingNum = Time.deltaTime * speedRate / flightTime;    //処理の回数。
+        var movementOneFrame = distance * processingNum;                //1Frameあたりの移動量。
+
+
+        for (var i = 0f; i < 1; i += (Time.deltaTime * speedRate / flightTime))
+        {
+            ball.transform.position += movementOneFrame;
+            yield return null; //1フレーム経過
+        }
+        ball.transform.position += endPos;
+
     }
 }
---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    [SerializeField] Transform endPos;  //終点座標
+    [SerializeField] float flightTime = 2;  //到達時間。
+    [SerializeField] float speedRate = 1;   //滞空時間を基準とした移動速度倍率
+    private const float gravity = -9.8f;    //重力
+    [SerializeField] float heightRate = 1;  //投てきの高さの倍率。
+    /// <summary>
+    /// 射出するオブジェクト
+    /// </summary>
+    [SerializeField, Tooltip("射出するオブジェクトをここに割り当てる")]
+    private GameObject ThrowingObject;
+
+    //攻撃タイプ。
+    enum AttackType{
+        enFrontAttack,            //正面攻撃。
+        enUpAttack,               //上攻撃。
+        enRightAttack,            //右攻撃。
+        enLeftAttack,             //左攻撃
+        enAttackTypeNum           //攻撃の種類の数。
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //todo TEST.
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Attack(AttackType.enLeftAttack);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Attack(AttackType.enUpAttack);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Attack(AttackType.enFrontAttack);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Attack(AttackType.enRightAttack);
+        }
+
+    }
+
+    /// <summary>
+    /// 攻撃。
+    /// </summary>
+    /// <param name="attackNum">攻撃の番号</param>
+    void Attack(AttackType attackNum)
+    {
+        //攻撃パターンが存在していたなら。
+        if (attackNum < AttackType.enAttackTypeNum)
+        {
+            //todo　ここに共通の処理を入れてもいいかも。
+
+            switch (attackNum)
+            {
+                case AttackType.enFrontAttack:    //前方攻撃のとき。
+
+                    StartCoroutine(FrontAttack(endPos.position, flightTime, speedRate));
+
+                    break;
+
+                case AttackType.enUpAttack:         //上攻撃のとき。
+
+                    StartCoroutine(ParabolaAttack(AttackType.enUpAttack, endPos.position, flightTime, speedRate, gravity * heightRate));
+
+                    break;
+
+                case AttackType.enRightAttack:
+
+                    StartCoroutine(ParabolaAttack(AttackType.enRightAttack, endPos.position, flightTime, speedRate, gravity * heightRate));
+
+                    break;
+
+                case AttackType.enLeftAttack:
+
+                    StartCoroutine(ParabolaAttack(AttackType.enLeftAttack, endPos.position, flightTime, speedRate, gravity * heightRate));
+
+                    break;
+            }
+        }
+    }
+    /// <summary>
+    /// 前方攻撃。
+    /// </summary>
+    /// <param name="endPos">玉の終着点</param>
+    /// <param name="flightTime">敵に玉が当たるまでの時間</param>
+    /// <param name="speedRate">玉のスピード倍率</param>
+    /// <returns>フレーム</returns>
+    private IEnumerator FrontAttack(Vector3 endPos, float flightTime, float speedRate)
+    {
+        // Ballオブジェクトの生成
+        GameObject ball = Instantiate(ThrowingObject, transform.position, Quaternion.identity);
+
+        var startPos = this.transform.position;                         //初期座標。
+        var distance = endPos - startPos;                               //玉までの距離。    
+        var processingNum = Time.deltaTime * speedRate / flightTime;    //処理の回数。
+        var movementOneFrame = distance * processingNum;                //1Frameあたりの移動量。
+
+        //玉の処理。
+        for (var i = 0f; i < 1; i += (Time.deltaTime * speedRate / flightTime))
+        {
+            ball.transform.position += movementOneFrame;
+            yield return null; //1フレーム経過
+        }
+
+        ball.transform.position += endPos;
+    }
+
+    /// <summary>
+    /// 放物線を描く敵の攻撃処理。
+    /// </summary>
+    /// <param name="attackNum">攻撃タイプ</param>
+    /// <param name="endPos">攻撃の終着点</param>
+    /// <param name="flightTime">敵に玉が当たるまでの時間</param>
+    /// <param name="speedRate">玉のスピード倍率</param>
+    /// <param name="gravity">攻撃にかかる重力</param>
+    /// <returns></returns>
+    private IEnumerator ParabolaAttack(AttackType attackNum, Vector3 endPos, float flightTime, float speedRate, float gravity)
+    {
+        // Ballオブジェクトの生成
+        GameObject ball = Instantiate(ThrowingObject, this.transform.position, Quaternion.identity);
+
+        var startPos = transform.position; // 初期位置
+
+        var diffX = (endPos - startPos).x; // 始点と終点のx成分の差分
+        var diffY = (endPos - startPos).y; // 始点と終点のy成分の差分
+        var diffZ = (endPos - startPos).z; // 始点と終点のz成分の差分
+
+        var vnx = (diffX - gravity * 0.5f * flightTime * flightTime) / flightTime; // 鉛直方向の初速度vn
+        var vny = (diffY - gravity * 0.5f * flightTime * flightTime) / flightTime; // 鉛直方向の初速度vn
+        var vnz = (diffZ - gravity * 0.5f * flightTime * flightTime) / flightTime; // 鉛直方向の初速度vn
+
+        //放物線を描く玉の処理。
+        for (var t = 0f; t < flightTime; t += (Time.deltaTime * speedRate))
+        {
+            var p = Vector3.Lerp(startPos, endPos, t / flightTime);   //水平方向の座標を求める (x,z座標)
+
+            switch(attackNum){
+
+                case AttackType.enUpAttack:       //上攻撃。
+                    p.y = startPos.y + vny * t + 0.5f * gravity * t * t; // 鉛直方向の座標y
+                    p.y = startPos.y + vny * t + 0.5f * gravity * t * t; // 鉛直方向の座標y
+                    break;
+
+                case AttackType.enRightAttack:    //右攻撃のとき。
+                    //X成分の差のほうが大きいとき。
+                    if (Mathf.Abs(diffX) > Mathf.Abs(diffZ))
+                    {
+                        p.x = startPos.x - vnx * t - 0.5f * gravity * t * t; // 鉛直方向の座標x
+                        continue;
+                    }
+                    //Z成分のほうが差が大きいとき。
+                    if (Mathf.Abs(diffX) < Mathf.Abs(diffZ)) 
+                    {
+                        
+                        p.z = startPos.z - vnz * t - 0.5f * gravity * t * t; // 鉛直方向の座標z
+                    }
+                        break;
+
+                case AttackType.enLeftAttack:     //左攻撃のとき。
+                    //X成分の差のほうが大きいとき。
+                    if (Mathf.Abs(diffX) > Mathf.Abs(diffZ))
+                    {
+                        p.x = startPos.x + vnx * t + 0.5f * gravity * t * t; // 鉛直方向の座標x
+                    }
+                    //Z成分のほうが差が大きいとき。
+                    if (Mathf.Abs(diffX) < Mathf.Abs(diffZ))
+                    {
+                        p.z = startPos.z - vnz * t - 0.5f * gravity * t * t; // 鉛直方向の座標z
+                    }
+                        break;
+            }
+
+            ball.transform.position = p;
+            yield return null; //1フレーム経過
+        }
+
+        // 終点座標へ補正
+        ball.transform.position = endPos;
+    }
+}
+
